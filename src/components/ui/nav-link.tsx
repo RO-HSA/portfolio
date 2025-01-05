c0c1c5e1c48312173aeb592913006e266575507e
Robert Santos <robertdahora@gmail.com>
2025-01-05 19:23:54 +0000

    feat: add nav-link component

---
+++ src/components/ui/nav-link.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { FC, ReactNode } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { GoHome } from 'react-icons/go';
+
+import { cn } from '@/lib/utils';
+
+interface NavLinkProps {
+  link: string;
+  children: ReactNode;
+}
+
+const NavLink: FC<NavLinkProps> = ({ children, link }) => {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={cn(
+        'flex justify-start py-2 px-2 max-w-[218px] rounded-md h-12 w-full transition-colors',
+        pathname === link
+          ? 'bg-foreground/20 text-primary'
+          : 'hover:bg-foreground/20 hover:text-primary',
+      )}
+      href={link}
+    >
+      <div className="flex items-center gap-1 text-sm">
+        <GoHome />
+        {children}
+      </div>
+    </Link>
+  );
+};
+
+export default NavLink;
