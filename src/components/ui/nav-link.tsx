62d2d1475e846d686766f46c6b4b1f4eade28522
Robert Santos <robertdahora@gmail.com>
2025-01-05 20:24:01 +0000

    feat: complements nav-link behavior when pathname equals url

--- src/components/ui/nav-link.tsx
+++ src/components/ui/nav-link.tsx
@@ -20,7 +20,7 @@ const NavLink: FC<NavLinkProps> = ({ children, link }) => {
       className={cn(
         'flex justify-start py-2 px-2 max-w-[218px] rounded-md h-12 w-full transition-colors',
         pathname === link
-          ? 'bg-foreground/20 text-primary'
+          ? 'bg-foreground/20 text-primary hover:bg-foreground/10 hover:text-primary/90'
           : 'hover:bg-foreground/20 hover:text-primary',
       )}
       href={link}
